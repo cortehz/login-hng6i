2dc3fd111469bc5be0be328aeb13c304753f834f
Samuel Omanchi <omanchis@gmail.com>
2019-09-16 20:45:12 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+
+import { withAuthenticator } from "aws-amplify-react-native";
+
+Amplify.configure(config);
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome to my HNGi6 Login Page</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+export default withAuthenticator(App, true);
